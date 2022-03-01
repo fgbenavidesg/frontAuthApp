368516e831e581052eb2d180e8624cc929ec8332
gregorio <fgbenavidesg@hotmail.com>
2022-03-01 15:17:16 -0500

    front appAuth

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { ProtectedModule } from './protected/protected.module';
+import { ValidarTokenGuard } from './guards/validar-token.guard';
+
+const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule) 
+  },
+  {
+    path: 'dashboard',
+    loadChildren: ()=> import('./protected/protected.module').then(m => m.ProtectedModule),
+    canActivate: [ ValidarTokenGuard],
+    canLoad: [ValidarTokenGuard] 
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{
+
+     useHash: false
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
